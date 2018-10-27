e684d7093e8ad6dad5bcf5fbdaa3c79ddef0e3e6
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-10-27 14:54:51 +0900

    session & session store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import bodyParser from "body-parser";
 import api from "./routes/index";
 import mysql from "mysql";
 import mysqlKey from "../keys/mysqlKey";
+import session from "express-session";
+import sessionKey from "../keys/sessionKey";
+import mysqlStoreKey from "../keys/mysqlStoreKey";
+var MySQLStroe = require("express-mysql-session")(session);
+const sessionStore = new MySQLStroe(mysqlStoreKey);
 
 //app
 const app = express();
@@ -22,6 +27,7 @@ let port = 3000;
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session(sessionKey));
 
 //settings
 app.set("view engine", "ejs");
@@ -33,6 +39,10 @@ app.use("/api", api);
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+//exports
+
+export { sessionStore };
 
 //listen
 app.listen(port, () => {
